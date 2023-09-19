1eb507767fbfd338111621179e9916e46bef7af0
almalmalm <almostkalinin@gmail.com>
2023-09-19 23:06:36 +0400

    fix: fixe time for displaying the loader

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,29 +1,38 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Info } from './info/Info';
 import { Navbar } from './navbar/Navbar';
 import { useWindowDimensions } from '@/app/hooks/hooks';
 import { Loader } from '../loader/Loader';
 
 export const Header = () => {
-  const { width, height } = useWindowDimensions();
-  if (width === undefined) {
+  const { width } = useWindowDimensions();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoading) {
     return <Loader />;
+  }
+
+  if (width! < 1400) {
+    return (
+      <>
+        <Info mobile={true} />
+        <Navbar mobile={true} />
+      </>
+    );
   } else {
-    if (width < 1400) {
-      return (
-        <>
-          <Info mobile={true} />
-          <Navbar mobile={true} />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Info />
-          <Navbar />
-        </>
-      );
-    }
+    return (
+      <>
+        <Info />
+        <Navbar />
+      </>
+    );
   }
 };
